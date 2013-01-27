6ccab552bfd452023dc3564a84977c373c81f926
Michał Konieczny <mic22@mic22.info>
2013-01-27 19:45:08 +0100

    zdef. 3x int a,b,c i przypisz im 1,5,8. Za pomocą jednego wskaźnika dokanaj zmian aby ich wartości wynosiły kolejno a+a b-5 oraz c/2

--- PowtorkaDoKolokwium/PowtorkaDoKolokwium/main.cpp
+++ PowtorkaDoKolokwium/PowtorkaDoKolokwium/main.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 int main()
 {
-	int zmienna=10;
-	int *wsk0=&zmienna;
-	int *wsk1=wsk0;
+	int a, b, c;
+	a=1;
+	b=5;
+	c=8;
+	int *wsk;
 
-	cout<<"adres zmiennej: "<<&zmienna<<endl;
-	cout<<"wskaznik 0 pokazuje na "<<wsk0<<endl;
-	cout<<"wskaznik 1 pokazuje na "<<wsk1<<endl;
-	cout<<"adres wskaznika 0"<<&wsk0<<endl;
-	cout<<"adres wskaznika 1"<<&wsk1<<endl;
+	wsk=&a;
+	*wsk+=1;
+
+	wsk=&b;
+	*wsk-=5;
+
+	wsk=&c;
+	*wsk/=2;
+
+	cout<<a<<b<<c<<endl;
 
 	return 0;
 }
