85eed886f443c0aaf57aef7bbdbe0c752f75623d
Michał Konieczny <mic22@mic22.info>
2013-01-27 19:52:32 +0100

    program realizujący schemat 27.6@w7.pdf

--- PowtorkaDoKolokwium/PowtorkaDoKolokwium/main.cpp
+++ PowtorkaDoKolokwium/PowtorkaDoKolokwium/main.cpp
@@ -5,19 +5,17 @@ using namespace std;
 int main()
 {
 	int a=1;
-	int *w1, *w2, *w3, *w4;
-
+	int b;
+	int *w1, *w2, *w3, *w4, *w5;
+	
 	w1=&a;
 	w2=&a;
-	w3=&a;
-	w4=&a;
-
-	// #2
-
-	w1=&a;
-	w2=w1;
 	w3=w2;
-	w4=w3;
+	w5=w3;
+	w4=w5;
+	b=*w4;
+
+	cout<<b<<endl;
 
 	return 0;
 }
