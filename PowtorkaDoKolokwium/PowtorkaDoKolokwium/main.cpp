caa3947f01afb948c596e3b863754b7d08c5d5e3
Michał Konieczny <mic22@mic22.info>
2013-01-27 19:36:02 +0100

    init & zdefiniuj i zainicjalizuj dwa wskaźniki adresem tej samej zmiennej. Pokaż na ekranie adres zmiennej, adresy, które przechowują wskaźniki oraz adresy wskaźników

---
+++ PowtorkaDoKolokwium/PowtorkaDoKolokwium/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int zmienna=10;
+	int *wsk0=&zmienna;
+	int *wsk1=&zmienna;
+
+	cout<<"adres zmiennej: "<<&zmienna<<endl;
+	cout<<"wskaznik 0 pokazuje na "<<wsk0<<endl;
+	cout<<"wskaznik 1 pokazuje na "<<wsk1<<endl;
+	cout<<"adres wskaznika 0"<<&wsk0<<endl;
+	cout<<"adres wskaznika 1"<<&wsk1<<endl;
+
+	return 0;
+}
