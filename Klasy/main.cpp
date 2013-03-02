f903787ab07c2d264a4c5d966a64043ebbd5d6cd
Michał Konieczny <mic22@mic22.info>
2013-03-02 16:18:24 +0100

    zadanie z klas

--- Klasy/main.cpp
+++ Klasy/main.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+int gnk=100;
 
-    return 0;
+class Bankomat
+{
+public:
+    Bankomat(string);
+    bool wyplac(double);
+    bool wplac(double);
+    double getStan() const;
+    
+private:
+    int numer;
+    string nazwisko;
+    double stan;
+};
+
+Bankomat::Bankomat(string nazwa)
+{
+    this->nazwisko=nazwa;
+    this->numer=gnk++;
+    this->stan=100;
 }
 
+bool Bankomat::wyplac(double kwota)
+{
+    if(this->stan>=kwota)
+    {
+        this->stan-=kwota;
+        return true;
+    }
+    return false;
+}
+
+bool Bankomat::wplac(double kwota)
+{
+    this->stan+=kwota;
+    return true;
+}
+
+double Bankomat::getStan() const
+{
+    return this->stan;
+}
+
+int main()
+{
+    Bankomat *a=new Bankomat("Nazwisko");
+    cout<<a->wyplac(60)<<endl;
+    cout<<a->wplac(100)<<endl;
+    cout<<a->getStan()<<endl;
+    
+    return 0;
+}
