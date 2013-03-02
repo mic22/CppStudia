1c0ef76681027b41da07a2088cf9bc6ebca62c2d
Michał Konieczny <mic22@mic22.info>
2013-03-02 15:30:01 +0100

    kod z wykładów

--- Struktury/Struktury/main.cpp
+++ Struktury/Struktury/main.cpp
@@ -20,13 +20,14 @@ struct Node
 	//pola danych
 	Zwierzatko data;
 
-	//pola obs³ugi struktury danych 
+	//pola obs³ugi struktury danych
 	Node *next;
 	Node *at(const int); //odwolanie do elementu na konkretnej pozycji
 	Node *at(); //odwolanie do ostatniego elementu
-	int static i; //przechwouje aktualny rozmiar struktury
+	unsigned int max; //przechwouje aktualny rozmiar struktury
 	int inc(); //tworzy kolejne element listy i zwraca aktualny rozmiar
 	Node();
+    int del(const int); //usuwa elementy poczawszy od wskazanego indeksem do konca
 };
 
 int main()
@@ -51,9 +52,22 @@ int main()
 		tmp->wiek=16;
 		tmp->plec=false;
 
+    bool b=true;
+    while(b)
+    {
+        zwierzatka->inc();
+        tmp=&zwierzatka->at()->data;
+        cout<<"Podaj nazwe (str), wiek (int), plec (bool):"<<endl;
+        cin>>tmp->nazwa>>tmp->wiek>>tmp->plec;
+        cout<<"Kontynuowac wprowadzanie? (bool): ";
+        cin>>b;
+    }
+
 	zwierzatka->at(2)->data.nazwa="Walen"; //modyfikacja danych 3 elementu struktury
 
-	for(int k=0; k<=Node::i; k++) //przejœcie po elementach struktury i wyswietlenie danych
+    zwierzatka->del(2); //usuwanie (c, this->max)
+        
+	for(int k=0; k<=zwierzatka->max; k++) //przejœcie po elementach struktury i wyswietlenie danych
 	{
 		tmp=&zwierzatka->at(k)->data;
 			cout<<k+1<<". "<<tmp->nazwa<<" (wiek: "<<tmp->wiek<<", plec: "<<(tmp->plec?"M":"F")<<")"<<endl;
@@ -62,9 +76,7 @@ int main()
 	return 0;
 }
 
-int Node::i=0;
-
-Node *Node::at() { return this->at(Node::i); }
+Node *Node::at() { return this->at(this->max); }
 
 Node *Node::at(const int a)
 {
@@ -84,8 +96,16 @@ Node *Node::at(const int a)
 
 int Node::inc()
 {
-	this->at(Node::i)->next=new Node;
-	return ++Node::i;
+	this->at(this->max)->next=new Node;
+	return ++this->max;
 }
 
 Node::Node() { this->next=nullptr; } //do czasu wykonania ::inc() pole ::next nie wskazuje nastêpnika
+
+int Node::del(const int a)
+{
+	delete this->at(this->max)->next;
+    for(int i=this->max; i>a; i--, this->max--)
+        delete this->at(this->max);
+	return this->max;
+}
