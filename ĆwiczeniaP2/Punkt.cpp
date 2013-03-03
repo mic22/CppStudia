d0820eadb5a0bc9fb99d38eb7b8c215cb41b5e54
Michał Konieczny <mic22@mic22.info>
2013-03-03 12:28:29 +0100

    uproszczenie

--- ĆwiczeniaP2/Punkt.cpp
+++ ĆwiczeniaP2/Punkt.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 Punkt::Punkt()
 {
-    this->x=0;
-    this->y=0;
+    x=0;
+    y=0;
 }
 
 Punkt::Punkt(double x, double y)
@@ -17,18 +17,18 @@ Punkt::Punkt(double x, double y)
 
 Punkt::Punkt(const Punkt &obj)
 {
-    this->x=obj.x;
-    this->y=obj.y;
+    x=obj.x;
+    y=obj.y;
 }
 
 Punkt::~Punkt() {}
 
 void Punkt::setX(double x) { this->x=x; }
 void Punkt::setY(double y) { this->y=y; }
-double Punkt::getX() const { return this->x; }
-double Punkt::getY() const { return this->y; }
+double Punkt::getX() const { return x; }
+double Punkt::getY() const { return y; }
 
 void Punkt::display() const
 {
-    cout<<"("<<this->x<<", "<<this->y<<")"<<endl;
+    cout<<"("<< x <<", "<< y <<")"<<endl;
 }
